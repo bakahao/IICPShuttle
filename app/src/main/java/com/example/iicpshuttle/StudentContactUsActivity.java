7e57fb94592738c87f356ece6b58b6d44a7a283b
bakahao <shchng43@gmail.com>
2023-10-04 08:27:16 +0800

    Done Contact US (Admin & Student)

--- app/src/main/java/com/example/iicpshuttle/StudentContactUsActivity.java
+++ app/src/main/java/com/example/iicpshuttle/StudentContactUsActivity.java
@@ -17,17 +17,16 @@ public class StudentContactUsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_contact_us);
 
         ImageView backIconImageView = findViewById(R.id.backIconImageView);
-// 从SharedPreferences中读取数据
+
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
         String userEmail = sharedPreferences.getString("email", ""); // 读取邮箱数据
         String userPhone = sharedPreferences.getString("phone", ""); // 读取电话数据
 
-// 在另一个页面中，将数据显示在相应的视图中
-        TextView emailTextView = findViewById(R.id.showEmail); // 假设你有一个TextView来显示邮箱
-        TextView phoneTextView = findViewById(R.id.showPhone); // 假设你有一个TextView来显示电话
+        TextView emailTextView = findViewById(R.id.showEmail);
+        TextView phoneTextView = findViewById(R.id.showPhone);
 
-        emailTextView.setText(userEmail); // 将邮箱数据设置到TextView中
-        phoneTextView.setText(userPhone); // 将电话数据设置到TextView中
+        emailTextView.setText(userEmail); // replace the email to userEmail(textView)
+        phoneTextView.setText(userPhone); // replace the phone to userEmail(textView)
 
 
         backIconImageView.setOnClickListener(v -> {
