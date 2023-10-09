4ed5bee7990c10b0375037e4c105be808b4f9692
cf <junchfoong@gmail.com>
2023-10-09 11:56:40 +0800

    Link to firebase realtime database

--- app/src/main/java/com/example/iicpshuttle/LoginActivity.java
+++ app/src/main/java/com/example/iicpshuttle/LoginActivity.java
@@ -14,11 +14,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends Activity {
 
     private EditText emailEditText, passwordEditText;
-    FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +35,6 @@ public class LoginActivity extends Activity {
         emailEditText = findViewById(R.id.idEditText);
         passwordEditText = findViewById(R.id.idEditText2);
         Button loginButton = findViewById(R.id.button2);
-
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
 
         Button registerButton = findViewById(R.id.button1);
 
@@ -48,27 +52,18 @@ public class LoginActivity extends Activity {
                 //Display error message
                 Toast.makeText(LoginActivity.this, "Please enter Student ID and Password", Toast.LENGTH_SHORT).show();
             } else {
-                if (email.equals("admin") && enteredPassword.equals("admin")) {
-                    Intent intent = new Intent(LoginActivity.this, AdminHomePageActivity.class);
-                    startActivity(intent);
-                }else if(email.equals("driver") && enteredPassword.equals("driver")){
-                    Intent intent = new Intent(LoginActivity.this, DriverHomePageActivity.class);
-                    startActivity(intent);
-                }else {
-                    // Check the input exist in database or not
-                    mAuth.signInWithEmailAndPassword(email, enteredPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Toast.makeText(LoginActivity.this, "User log in successfully", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-                            } else {
-                                Toast.makeText(LoginActivity.this, "Log in Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                // Check the input exist in database or not
+                mAuth.signInWithEmailAndPassword(email, enteredPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            checkRole();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Log in Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
 
-                            }
                         }
-                    });
-                }
+                    }
+                });
             }
         });
     }
@@ -78,7 +73,46 @@ public class LoginActivity extends Activity {
         super.onStart();
         FirebaseUser user = mAuth.getCurrentUser();
         if (user != null){
-            startActivity(new Intent(this, HomeActivity.class));
+            checkRole();
         }
+    }
+
+    private void checkRole(){
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        FirebaseUser mUser = mAuth.getCurrentUser();
+
+        databaseReference.child(mUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    String role = snapshot.child("userRole").getValue(String.class);
+
+                    if (role != null){
+                        if (role.equals("Student")){
+                            Toast.makeText(LoginActivity.this, "User log in successfully", Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                        } else if (role.equals("Admin")){
+                            Toast.makeText(LoginActivity.this, "User log in successfully", Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(LoginActivity.this, AdminHomePageActivity.class));
+                        } else if (role.equals("Driver")) {
+                            Toast.makeText(LoginActivity.this, "User log in successfully", Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(LoginActivity.this, DriverHomePageActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Role not found for this user", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "User not found in the database", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(LoginActivity.this, "Database Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/iicpshuttle/RegisterActivity.java
+++ app/src/main/java/com/example/iicpshuttle/RegisterActivity.java
@@ -15,6 +15,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -29,70 +32,91 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
 
+        registerButton = findViewById(R.id.button1);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+
+    private void register(){
         nameEditText = findViewById(R.id.nameEditText);
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         phoneEditText = findViewById(R.id.phoneEditText);
-        registerButton = findViewById(R.id.button1);
+
 
         nameErrorTextView = findViewById(R.id.nameErrorTextView);
         studentIdErrorTextView = findViewById(R.id.studentIdErrorTextView);
         passwordErrorTextView = findViewById(R.id.passwordErrorTextView);
         phoneErrorTextView = findViewById(R.id.phoneErrorTextView);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        // 获取用户输入
+        String name = nameEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+        String phoneNumber = phoneEditText.getText().toString().trim();
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty() || phoneNumber.isEmpty()) {
+            if (name.isEmpty()) {
+                nameErrorTextView.setVisibility(View.VISIBLE);
+            } else {
+                nameErrorTextView.setVisibility(View.GONE);
+            }
+
+            if (email.isEmpty()) {
+                studentIdErrorTextView.setVisibility(View.VISIBLE);
+            } else {
+                studentIdErrorTextView.setVisibility(View.GONE);
+            }
+
+            if (password.isEmpty()) {
+                passwordErrorTextView.setVisibility(View.VISIBLE);
+            } else {
+                passwordErrorTextView.setVisibility(View.GONE);
+            }
+
+            if (phoneNumber.isEmpty()) {
+                phoneErrorTextView.setVisibility(View.VISIBLE);
+
+            } else {
+                phoneErrorTextView.setVisibility(View.GONE);
+            }
+            return;
+        }
+
+        String role = "Student";
+        User user = new User(name, role, email, phoneNumber);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View v) {
-                // 获取用户输入
-                String name = nameEditText.getText().toString().trim();
-                String email = emailEditText.getText().toString().trim();
-                String password = passwordEditText.getText().toString().trim();
-                String phoneNumber = phoneEditText.getText().toString().trim();
-                if (name.isEmpty() || email.isEmpty() || password.isEmpty() || phoneNumber.isEmpty()) {
-                    if (name.isEmpty()) {
-                        nameErrorTextView.setVisibility(View.VISIBLE);
-                    } else {
-                        nameErrorTextView.setVisibility(View.GONE);
-                    }
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseUser mUser = mAuth.getCurrentUser();
+                    DatabaseReference userRef = FirebaseDatabase.getInstance("https://iicpshuttle-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+                    userRef.child("Users").child(mUser.getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
 
-                    if (email.isEmpty()) {
-                        studentIdErrorTextView.setVisibility(View.VISIBLE);
-                    } else {
-                        studentIdErrorTextView.setVisibility(View.GONE);
-                    }
-
-                    if (password.isEmpty()) {
-                        passwordErrorTextView.setVisibility(View.VISIBLE);
-                    } else {
-                        passwordErrorTextView.setVisibility(View.GONE);
-                    }
-
-                    if (phoneNumber.isEmpty()) {
-                        phoneErrorTextView.setVisibility(View.VISIBLE);
-
-                    } else {
-                        phoneErrorTextView.setVisibility(View.GONE);
-                    }
-                    return;
-                }
-
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isComplete()){
-                            Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "Registration Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            if (task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Registration Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
                         }
+                    });
 
-                    }
-                });
+
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Registration Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
     }
-    public void onIconClick(View view) {
+    private void onIconClick(View view) {
         // 创建 Intent 以启动 LoginActivity
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
