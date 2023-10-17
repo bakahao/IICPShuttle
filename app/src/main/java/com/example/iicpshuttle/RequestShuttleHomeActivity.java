f0425d80725893c1d457c8bdb7c08351164d803a
cf <junchfoong@gmail.com>
2023-10-18 00:56:26 +0800

    scan me page and request shuttle

---
+++ app/src/main/java/com/example/iicpshuttle/RequestShuttleHomeActivity.java
@@ -0,0 +1,24 @@
+package com.example.iicpshuttle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RequestShuttleHomeActivity extends AppCompatActivity {
+
+    private Button requestShuttle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_shuttle_home);
+
+        requestShuttle = findViewById(R.id.button1);
+
+        requestShuttle.setOnClickListener(view -> {
+            startActivity(new Intent(this, RequestShuttleActivity.class));
+        });
+    }
+}
