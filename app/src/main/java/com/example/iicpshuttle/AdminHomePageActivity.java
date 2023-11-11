fbbea9fbec8bcb1e137b03b128a16f53bdadcac8
TanYanWai <144647378+TanYanWai@users.noreply.github.com>
2023-11-11 17:23:28 +0800

    Admin Reminder

--- app/src/main/java/com/example/iicpshuttle/AdminHomePageActivity.java
+++ app/src/main/java/com/example/iicpshuttle/AdminHomePageActivity.java
@@ -7,18 +7,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class AdminHomePageActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<ScheduleAdmin> list;
+    DatabaseReference databaseReference;
+    AdapterAdmin adapter;
     FirebaseAuth mAuth;
     Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_homepage);
+
+
+
 
         AppCompatButton button5 = findViewById(R.id.button5);
         AppCompatButton buttonManageScheduleHostel = findViewById(R.id.buttonManageScheduleHostel);
@@ -31,9 +48,8 @@ public class AdminHomePageActivity extends AppCompatActivity {
             startActivity(new Intent(AdminHomePageActivity.this, LoginActivity.class));
         });
 
-        AppCompatButton buttonManageScheduleHostel = findViewById(R.id.buttonManageScheduleHostel);
 
-        AppCompatButton buttonManageScheduleHostel = findViewById(R.id.buttonManageScheduleHostel);
+
 
         AppCompatButton button4 = findViewById(R.id.button4);
 
@@ -46,13 +62,6 @@ public class AdminHomePageActivity extends AppCompatActivity {
             }
         });
 
-        buttonManageScheduleHostel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminHomePageActivity.this, ManageScheduleHostelActivity.class);
-                startActivity(intent);
-            }
-        });
 
         buttonManageScheduleHostel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,13 +71,7 @@ public class AdminHomePageActivity extends AppCompatActivity {
             }
         });
 
-        buttonManageScheduleHostel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminHomePageActivity.this, ManageScheduleHostelActivity.class);
-                startActivity(intent);
-            }
-        });
+
 
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +79,39 @@ public class AdminHomePageActivity extends AppCompatActivity {
                 Intent intent = new Intent(AdminHomePageActivity.this, DriverRegisterActivity.class);
                 startActivity(intent);
             }
+        });
+        
+
+        recyclerView = findViewById(R.id.RecyclerViewScheduleAdmin);
+        databaseReference  = FirebaseDatabase.getInstance("https://iicpshuttle-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("RequestShuttle");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        adapter = new AdapterAdmin(this,list);
+        recyclerView.setAdapter(adapter);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren())
+                {
+                    for (DataSnapshot requestSnapShot : dataSnapshot.getChildren()){
+                        ScheduleAdmin scheduleAdmin  = requestSnapShot.getValue(ScheduleAdmin.class);
+
+                        if (scheduleAdmin != null && "Pending".equals(scheduleAdmin.getStatus())) {
+                            list.add(scheduleAdmin);
+                        }
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("Firebase Error", "Error: " + error.getMessage());
+            }
+
         });
 
     }
